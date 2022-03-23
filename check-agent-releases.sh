a897b6c61a96abd52b318cf048ec6fef9cf2e374
Doğukan Çağatay <dcagatay@gmail.com>
2022-03-23 16:41:32 +0300

    Try jq fix

--- check-agent-releases.sh
+++ check-agent-releases.sh
@@ -8,6 +8,6 @@ _NUM_RELEASES="${NUM_RELEASES:-100}"
 curl -fsSN \
   -H "Accept: application/vnd.github.v3+json" \
   "https://api.github.com/repos/$_GIT_REPOSITORY_NAME/releases?per_page=$_NUM_RELEASES" |
-  jq -r -c '.[] | select(.prerelease==false) | .name ' |
+  jq -r '.[] | select(.prerelease==false) | .name ' |
   cut -d 'v' -f2
 # | jq  --raw-input . | jq --slurp .
