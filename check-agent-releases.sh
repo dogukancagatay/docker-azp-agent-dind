fadfa8d93ac1fb4f104cc420c5993eecdf23f740
Doğukan Çağatay <dcagatay@gmail.com>
2022-03-23 16:02:01 +0300

    Fix variable name in release checker script

--- check-agent-releases.sh
+++ check-agent-releases.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 _GIT_REPOSITORY_NAME="${REPO:-microsoft/azure-pipelines-agent}"
 _NUM_RELEASES="${NUM_RELEASES:-100}"
@@ -6,7 +7,7 @@ _NUM_RELEASES="${NUM_RELEASES:-100}"
 ## List releases (not prereleases)
 curl -fsSN \
   -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/$_REPO/releases?per_page=$_NUM_RELEASES" |
+  "https://api.github.com/repos/$_GIT_REPOSITORY_NAME/releases?per_page=$_NUM_RELEASES" |
   jq -r '.[] | select(.prerelease==false) | .name ' |
   cut -d 'v' -f2
 # | jq  --raw-input . | jq --slurp .
