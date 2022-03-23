676fb0862ac27db7740122bbecef9983dfdca62d
Doğukan Çağatay <dcagatay@gmail.com>
2022-03-23 15:42:32 +0300

    Make release checker usable by workflow

--- check-agent-releases.sh
+++ check-agent-releases.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-REPO="microsoft/azure-pipelines-agent"
+_GIT_REPOSITORY_NAME="${REPO:-microsoft/azure-pipelines-agent}"
+_NUM_RELEASES="${NUM_RELEASES:-100}"
 
 ## List releases (not prereleases)
-curl \
+curl -fsSN \
   -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/$REPO/releases?per_page=100" | jq '.[] | select(.prerelease==false) | .name'
+  "https://api.github.com/repos/$_REPO/releases?per_page=$_NUM_RELEASES" |
+  jq -r '.[] | select(.prerelease==false) | .name ' |
+  cut -d 'v' -f2
+# | jq  --raw-input . | jq --slurp .
